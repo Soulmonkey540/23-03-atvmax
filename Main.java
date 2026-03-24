1f5353a962c85fc72259d9f5f129b7b84a0a6ead
Soulmonkey540 <gabrielpm1605@gmail.com>
2026-03-24 14:39:46 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main {
 	    Carro c = new Carro("Fusca", 40.0);
 	    System.out.println("Modelo: "+ c.getModelo());
 	    System.out.println("Velocidade: " + c.getVelocidade()+"km/h");
+	    c.setVelocidade(50.0);
+	    System.out.println("Velocidade: " + c.getVelocidade()+"km/h");
 	}
 }
